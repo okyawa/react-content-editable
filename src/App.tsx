081335afea4876c2a4ccc31097f0d0584a6fef1a
Hirokazu Okawa <h.okyawa@gmail.com>
2023-05-10 17:21:06 +0900

    modify: 表示調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
-import { useState } from "react";
+import { useState } from 'react';
 import ContentEditable from './content-editable'
 
 export default function App() {
 
-  const [text, setText] = useState<string>("ここは書き換え可能な文章です。");
-  const [name, setName] = useState<string>("山田太郎");
+  const [text, setText] = useState<string>('ここは書き換え可能な文章です。');
+  const [name, setName] = useState<string>('山田太郎');
 
   const handleTextBlur = () => {
-    console.log(text);
+    console.log('Textのbluerイベント - Stateの値: ', text);
   }
 
   const handleNameBlur = () => {
-    console.log(name);
+    console.log('Nameのbluerイベント - Stateの値: ', name);
   }
 
   return (
-    <div style={{'margin':'2em'}}>
-        <ContentEditable value={text} onChange={setText} onBlur={handleTextBlur}/>
-        <div>
-          Text:{text}
-        </div>
-        <ContentEditable value={name} onChange={setName} onBlur={handleNameBlur}/>
-        <div>
-          Name:{name}
-        </div>
+    <div style={{ maxWidth: '600px', margin: '2em auto' }}>
+      <h1>Reactでcontenteditableを使うサンプル</h1>
+      <hr />
+      <div style={{ backgroundColor: '#f5f5f5' }}>
+        <ContentEditable value={text} onChange={setText} onBlur={handleTextBlur} />
+      </div>
+      <div>
+        TextのStateの値:
+        <div style={{ color: '#666' }}>{text}</div>
+      </div>
+      <hr />
+      <div style={{ backgroundColor: '#f5f5f5' }}>
+        <ContentEditable value={name} onChange={setName} onBlur={handleNameBlur} />
+      </div>
+      <div>
+        NameのStateの値:
+        <div style={{ color: '#666' }}>{name}</div>
+      </div>
+      <hr />
     </div>
   );
 }
